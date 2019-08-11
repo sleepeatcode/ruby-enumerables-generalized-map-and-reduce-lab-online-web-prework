506f64814c7d30e41f5c35feba43601456b1b2bf
Rachel T <rachmaybe@gmail.com>
2019-08-11 22:22:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# Your Code Here
+def map(array)
+  new = []
+  # array.map{|n| n*-1}
+  counter = 0 
+  while counter < array.length do 
+    new.push(yield(array[counter])) 
+    counter +=1 
+  end 
+  return new  
+end 
+
+def reduce(source_array,start=0)
+  counter = 0 
+  while counter < source_array.length do
+    if source_array[counter] 
+      return true
+    end 
+    counter +=1
+  end 
+  return false
+end 
