3edb177f69b250f23af3eb0223a2ed04fd148859
Rachel T <rachmaybe@gmail.com>
2019-08-11 23:00:30 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(array)
+  new = []
+  # array.map{|n| n*-1}
+  counter = 0 
+  while counter < array.length do 
+    new.push(yield(array[counter])) 
+    counter +=1 
+  end 
+  return new  
+end 
+
+def reduce(source_array,start=nil)
+  if start 
+    counter = 0 
+    total = start
+  else 
+    total = source_array[0]
+    counter = 1 
+  end
+  while counter < source_array.length do
+    total = yield(total,source_array[counter])
+      counter +=1
+  end 
+  total
+end 
